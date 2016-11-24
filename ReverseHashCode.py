e43a9a78797d0faf1339a7cd224cd08851a9ebcd
harshul970 <harshul_j970@yahoo.com>
2016-11-24 15:50:35 +0530

    Update ReverseHashCode.py

--- ReverseHashCode.py
+++ ReverseHashCode.py
@@ -4,18 +4,24 @@ def HashFunction(x):
 
 	ResultStr=""
 	i=0
-	b=1
-	while b!=7:
+	b=8
+	while b>7:
 		a = x%37
 		b = x/37
+		if(a>15):
+			ResultStr = ">gnirtS hsaH tcerrocnI<"
+			break
+		elif(b<7):			
+			ResultStr = ">gnirtS hsaH tcerrocnI<"
+			break
 		ResultStr = ResultStr +letters[a]
 		x=b
-		i=i+1	
+		i=i+1
 
 	FinalString = ResultStr[::-1]
 	return FinalString
 
 
 
-HashString = 680131659347
+HashString = 1311
 print "String : "+HashFunction(HashString)
